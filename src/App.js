43d045857990679700d18f2dcb0443484934456c
Mikelanne <mikelanne.wood@gmail.com>
2022-06-14 16:39:50 -0400

    base user action working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import axios from 'axios'
+import Home from "./components/Home"
+import Dashboard from './components/Dashboard'
+import { Component } from 'react';
+import Registration from './components/auth/Registration'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+
+    this.handleLogin = this.handleLogin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  checkLoginstatus() {
+    axios.get("http://localhost:3001/logged_in", {withCredentials: true}).then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    }).catch(error => {
+      console.log("check login error", error)
+    })
+  }
+
+  componentDidMount(){
+    this.checkLoginstatus();
+  }
+
+  handleLogout() {
+    this.setState ({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    })
+  }
+
+  handleLogin(data) {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+              <Route 
+              exact path={"/"} 
+              render={props => (
+                <Home {...props} handleLogin={this.handleLogin} handleLogout={this.handleLogout} loggedInStatus={this.state.loggedInStatus} />
+              )}
+              />
+              <Route 
+              exact path={"/dashboard"} 
+              render={props => (
+                <Dashboard {...props} loggedInStatus={this.state.loggedInStatus} user={this.state.user} handleLogout={this.handleLogout} />
+              )}
+              />
+              <Route 
+              exact path={"/registration"} 
+              render={props => (
+                <Registration {...props} loggedInStatus={this.state.loggedInStatus} user={this.state.user} />
+              )}
+              />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
